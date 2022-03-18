d946da3fff14c7363796e0ebb232a2a5abbac66e
Felipe Rocha <felipemota.rocha@gmail.com>
2022-03-18 10:45:29 -0300

    useState

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+  const [theme, setTheme] = useState("dark");
+
+  const incrementCount = () => {
+    setCount((prevState) => prevState + 1);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>{count}</h1>
+      <button onClick={incrementCount}>Increment</button>
     </div>
   );
-}
+};
 
 export default App;
