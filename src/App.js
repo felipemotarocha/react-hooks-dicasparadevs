134c8b47fb9e042a4633dc9a0c51716281acec61
Felipe Rocha <felipemota.rocha@gmail.com>
2022-03-18 15:03:29 -0300

    useRef #02

--- src/App.js
+++ src/App.js
@@ -3,17 +3,21 @@ import { useEffect, useState, useRef } from "react";
 const App = () => {
   const [name, setName] = useState("");
 
-  const renders = useRef(0);
+  const inputRef = useRef();
 
-  useEffect(() => {
-    renders.current = renders.current + 1;
-  });
+  const focusInput = () => {
+    inputRef.current.focus();
+  };
 
   return (
     <div>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <input
+        ref={inputRef}
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
       <p>Hello! My name is {name}</p>
-      <p>Renders: {renders.current}</p>
+      <button onClick={focusInput}>Focus Input</button>
     </div>
   );
 };
