959f7764f95f3f82b6f832c3144b93114e01fede
Felipe Rocha <felipemota.rocha@gmail.com>
2022-03-19 16:50:54 -0300

    useReducer

--- src/App.js
+++ src/App.js
@@ -1,19 +1,55 @@
-import { useEffect, useState, useRef } from "react";
+import { useReducer, useState } from "react";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "add-task":
+      return {
+        ...state,
+        tasks: [...state.tasks, { name: action.payload, isCompleted: false }],
+        tasksCount: state.tasksCount + 1,
+      };
+    case "toggle-task":
+      return {
+        ...state,
+        tasks: state.tasks.map((item, index) =>
+          index === action.payload
+            ? { ...item, isCompleted: !item.isCompleted }
+            : item
+        ),
+      };
+    default:
+      return state;
+  }
+};
 
 const App = () => {
-  const [name, setName] = useState("");
+  const [state, dispatch] = useReducer(reducer, { tasks: [], tasksCount: 0 });
 
-  const previousName = useRef();
-
-  useEffect(() => {
-    previousName.current = name;
-  }, [name]);
+  const [inputValue, setInputValue] = useState("");
 
   return (
     <div>
-      <input value={name} onChange={(e) => setName(e.target.value)} />
-      <p>Hello! My name is {name}</p>
-      <p>And my name was {previousName.current}</p>
+      <input
+        value={inputValue}
+        onChange={(e) => setInputValue(e.target.value)}
+      />
+      <button
+        onClick={() => {
+          dispatch({ type: "add-task", payload: inputValue });
+          setInputValue("");
+        }}
+      >
+        Adicionar
+      </button>
+
+      {state.tasks.map((task, index) => (
+        <p
+          onClick={() => dispatch({ type: "toggle-task", payload: index })}
+          style={{ textDecoration: task.isCompleted ? "line-through" : "none" }}
+        >
+          {task.name}
+        </p>
+      ))}
     </div>
   );
 };
